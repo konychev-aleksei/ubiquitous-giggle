4528496e5d18088232921d9f4663d4dd9840bb4f
konychev-aleksei <konychev-aleksei@users.noreply.github.com>
2025-05-22 18:21:08 +0000

    deploy: 693cfa285b3a3f386e9b5cdf7c944e2dd938f73c

---
+++ remote/assets/__federation_expose_Button-PHwv8R8-.js
@@ -0,0 +1,7 @@
+import { j as jsxRuntimeExports } from './jsx-runtime-XI9uIe3W.js';
+
+function Button() {
+  return /* @__PURE__ */ jsxRuntimeExports.jsx("button", { children: "Remote Button" });
+}
+
+export { Button as default };
